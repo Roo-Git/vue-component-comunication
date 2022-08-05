b95140b9919860451b554cfdeb600001e487718b
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-05 12:42:57 +0200

    Added styles

--- src/components/resources/AddResource.vue
+++ src/components/resources/AddResource.vue
@@ -14,7 +14,7 @@
         <input name="link" id="link" type="url" />
       </div>
       <div>
-        <base-button></base-button>
+        <base-button type="submit">Add Resource</base-button>
       </div>
     </form>
   </base-card>
@@ -25,4 +25,30 @@ export default {
   name: "Add-Resource",
 };
 </script>
-<style scoped></style>
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
